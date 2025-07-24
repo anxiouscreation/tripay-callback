3705fb7c4ee314cdfffc0c6134ab00fe9fadecef
anxiouscreation <anxiouscreation@gmail.com>
2025-07-24 16:44:44 +0700

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,8 +1,7 @@
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    // Di sini kamu bisa proses callback, validasi signature, dll
-    console.log('Callback Tripay diterima');
-    res.status(200).json({ success: true });
+    console.log('Tripay Callback Received ✅');
+    res.status(200).json({ success: true, message: "Callback received." });
   } else {
     res.status(405).json({ error: 'Method Not Allowed' });
   }
